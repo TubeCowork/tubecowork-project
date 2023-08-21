a302dfd93fbf10fa81dfc3b1b0227cfc243fa7af
abhithory <abhithory@gmail.com>
2023-08-21 08:15:30 +0530

    project initialization

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+export default function auth() {
+    return <div>Auth</div>
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,26 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss"
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#4D92FF", //pink-500
+                "primary-hover": "#2372f1", //pink-500
+                "primary-bg": "#010220", //#09162e
+                secondary: "rgb(147 51 234)", //
+                "secondary-hover": "#a64ef5", //
+                third: "#5E17EB",
+                highlight: "#FCB040",
+                "text-color": "#FFFFFF",
+                "text-color-2": "#000",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
 export default config
