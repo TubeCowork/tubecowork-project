7da0bdbafe0a63b2963d92dc54ff8f280338a4a3
abhithory <abhithory@gmail.com>
2023-08-25 09:34:36 +0530

    channel page added

---
+++ src/app/(root)/dashboard/channel/[id]/page.tsx
@@ -0,0 +1,22 @@
+"use client"
+import React from 'react'
+import { useSearchParams } from 'next/navigation';
+
+function page() {
+    const searchParams = useSearchParams()
+    const channelid = searchParams.get('id');
+
+    if (!channelid) return
+
+    const getChannelDetails = () => {
+
+    }
+
+    return (
+        <div>
+            Owned channel
+        </div>
+    )
+}
+
+export default page
